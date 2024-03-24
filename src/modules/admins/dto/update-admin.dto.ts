ce7b712471d0714ea184d8c84b89c44963cd1c37
Ratul <ratul@Ratuls-MacBook-Pro.local>
2024-03-24 15:40:40 +0600

    admin update

--- src/modules/admins/dto/update-admin.dto.ts
+++ src/modules/admins/dto/update-admin.dto.ts
@@ -1,5 +1,19 @@
 
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateAdminDto } from './create-admin.dto';
+import { IsOptional, MaxLength, MinLength } from 'class-validator';
 export class UpdateAdminDto extends CreateAdminDto {
 
+
+    // password
+    @ApiProperty({
+        type: 'string',
+        example: '123456',
+        description: 'admin password',
+    })
+    // @IsNotEmpty()
+    @IsOptional()
+    @MinLength(6)
+    @MaxLength(255)
+    password: string;
 }
